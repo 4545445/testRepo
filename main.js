fef30ede54e1d18bf41d8f5c0ba23bf9811e0dda
Jason Oliver <jasonc.oliver@F1006-27-34162.bmcc.cuny.edu>
2015-10-05 17:11:21 -0400

    added var codes

---
+++ main.js
@@ -0,0 +1,17 @@
+var red;
+
+function setup(){
+    red = color(255,0,0);
+    blue = color(0,0,255)
+}
+
+function draw(){
+    if(mouseIsPressed){
+
+ line(pmouseX,pmouseY,mouseX,mouseY);
+}
+
+ function mousePressed(){
+     console.log('mousepressed');
+
+ }
